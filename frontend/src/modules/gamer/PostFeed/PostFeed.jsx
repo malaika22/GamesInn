aa142860514579bd2cd9fbcdcc7d79dfafb8ed00
waliya15 <waliyahamid@gmail.com>
2022-01-24 23:54:25 +0500

    added post feed component

--- frontend/src/modules/gamer/PostFeed/PostFeed.jsx
+++ frontend/src/modules/gamer/PostFeed/PostFeed.jsx
@@ -1,8 +1,81 @@
-import { React } from "react";
+import { React, useEffect } from "react";
+import data from './data';
 import "./styles.scss";
+import { Table } from "antd";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShippingFast } from '@fortawesome/free-solid-svg-icons'
 
 const PostFeed = () => {
-  return <div>Posts</div>;
+
+   function getRatings() {
+    for(let d of data){
+       // Get percentage
+       const starPercentage = (d.rating / 5) * 100;
+       console.log(d.postId)
+
+       // Round to nearest 10
+       const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+       console.log(starPercentageRounded)
+ 
+       // Set width of stars-inner to percentage
+      document.querySelector(`.post-${d.postId}.rating-stars-inner`).style.width = starPercentageRounded;
+    }}
+     
+    useEffect(()=>{
+      getRatings()
+    },[])
+  const columns=[
+    {
+      title:'Offer Title',
+      dataIndex:'titleContent',
+      render: title => <a href="">{title.join(' | ')}</a>
+    },
+    {
+      title:'Rating',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.rating - b.rating,
+      render: (data)=>
+     {
+      return <div className="rating">
+              <img src={data.image} alt="UserDispalyImage" />
+              <div className="rating-right">
+                <h3>{data.gamerName}</h3>
+                <div className='rating-stars-outer'>
+                  <div className={`post-${data.postId} rating-stars-inner`}>
+                  </div>
+                </div>
+              </div>
+            </div>
+    }
+    },
+    {
+      title:'Instant Delivery',
+      dataIndex:'',
+      render:()=> <>
+        <FontAwesomeIcon icon={faShippingFast} size="2x" />
+        <h4>fast delivery</h4>
+      </>
+     
+    },
+    {
+      title:'Price',
+      dataIndex:'price',
+      render: price => <>
+        <h3>${price}</h3>
+        <button>BUY NOW</button>
+      </>,
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.price - b.price,
+    }
+  ]
+  
+  return <div className="posts">
+   <Table
+   dataSource={data}
+   columns={columns}
+   pagination={{ pageSize: 20 }} scroll={{ y: 240 }}
+   ></Table>
+  </div>;
 };
 
 export default PostFeed;
---
+++ frontend/src/modules/gamer/PostFeed/data.js
@@ -0,0 +1,50 @@
+let data;
+export default  data =[
+    {
+        postId:1,
+        gamerName: 'Bertie Yates',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+        game:'Fortnite',
+        titleContent:['Level 1279', '129 Skins', 'Skull Trooper, Candy Axe, Rabbit Raider', 'Full Access - chicksfnc203' ],
+        rating: 5.0,
+        price:45,
+    },
+    {
+        postId:2,
+        gamerName: 'Hester Hogan',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+        game:'Fortnite',
+        titleContent:['[PC] Fortnite', '1479 Level', '120+ Outfits', 'Glow', 'Wonder', 'Minty Axe [chicksgfor10092]'],
+        rating: 4.3,
+        price: 30,
+    },
+    {
+        postId:3,
+        gamerName: 'Larry Little',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+        game:'Escape From Tarkov',
+        titleContent:['[Global] Escape From Tarkov', '(0 hours played)', 'Original Email'],
+        rating: 3.7,
+        price: 56,
+      
+    },
+    {
+        postId:4,
+        gamerName: 'Sean Walsh',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+        game:'Pubg',
+        titleContent:['LEVEL 58','CLOTH SET 27','GUN SKINS 16', 'HELMET 1','BAG 5 TWITTER LOGIN CHEAPEST AND SAFE INSTANT DELIVERY'],
+        rating: 2.9,
+        price: 63,
+    },
+    {
+        postId:5,
+        gamerName: 'Lola Gardner',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+        game:'Free Fire',
+        titleContent:[' Level 62', 'Diamond2 S24','Gold3 S10', '36 Characters', 'Gun Legend Skins', 'Many Amazing Items+Pets', '10 Times Elite', 'Safe&Legal'],
+        rating: 4,
+        price: 72,
+    },
+  ];
+  
