33c9cc07d98557c52b3b2edeafae4bbb18fbb2a1
waliya15 <waliyahamid@gmail.com>
2022-02-03 22:18:33 +0500

    Update index.js

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -4,7 +4,7 @@ import SignUp from "../components/AuthPages/SignUp";
 import SignIn from "../components/AuthPages/SignIn";
 import Home from "../components/HomeLayout/Home/Home";
 import GamerLayout from "../components/GamerLayout/GamerLayout";
-import Accounts from "../modules/gamer/Accounts/Accounts";
+// import Accounts from "../modules/gamer/Accounts/Accounts";
 import GamerPrivateRoute from "./GamerPrivateRoute";
 import PostFeed from "../modules/gamer/PostFeed/PostFeed";
 import Post from "../modules/gamer/PostFeed/Post";
@@ -18,7 +18,7 @@ const ModuleRoutes = () => {
       <Route exact path="/login" element={<SignIn />} />
       <Route exact path="/" element={<GamerPrivateRoute />}>
         <Route exact path="/postfeed" element={<PostFeed />} />
-        <Route exact path="/accounts" element={<Accounts />} />
+        {/* <Route exact path="/accounts" element={<Accounts />} /> */}
         <Route exact path="/post/:id" element={<Post />} />
       </Route>
     </Routes>
