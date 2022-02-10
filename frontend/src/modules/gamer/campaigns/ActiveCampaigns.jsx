dfbd702c71fb63fef5816c907db6a7bce64ac1d9
waliya15 <waliyahamid@gmail.com>
2022-02-10 11:41:40 +0500

    added campaign screens

---
+++ frontend/src/modules/gamer/campaigns/ActiveCampaigns.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { campaignsData } from './campaignsData'
+import {Card, Row, Col, Typography, Space, Descriptions,} from 'antd'
+import Meta from 'antd/lib/card/Meta';
+import './styles.scss'
+
+function ActiveCampaigns() {
+  const {Text, Title}= Typography;
+
+  return (
+    <>
+        <Row gutter={[24,24]}>
+            {campaignsData.map(campaign => (
+                 <Col xs={24} md={12} lg={8} >
+                    <Card
+                        bodyStyle={{paddingBlock: '10px'}}
+                        headStyle={{backgroundColor: '#213956'}}
+                        hoverable
+                        title={<Title style={{color: 'white'}} level={5}>{campaign.campaignName}</Title>}
+                        >
+                            <Meta 
+                                title='Description'
+                                description={campaign.campaignDescription}
+                            ></Meta>
+                            <div style={{marginTop: '10px',justifyContent: 'space-between', display:'flex'}} >
+                                <p >Target: {campaign.campaignTargetedAmount}</p>
+                                <p>{campaign.campaignDays} days</p>
+                            </div>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+    </>
+  )
+}
+
+export default ActiveCampaigns
---
+++ frontend/src/modules/gamer/campaigns/campaignsData.js
@@ -0,0 +1,56 @@
+export const campaignsData =[
+    {
+        campaignName: 'testing_campaign_1',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_2',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_3',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_4',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_5',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_6',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_7',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_8',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+    {
+        campaignName: 'testing_campaign_9',
+        campaignDays: 2,
+        campaignDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis praesentium ad consequatur doloribus ab quod suscipit quia quaerat magni voluptate quibusdam nesciunt sit facilis, delectus dolor repellendus vero molestiae.',
+        campaignTargetedAmount: 1000,
+    },
+]
