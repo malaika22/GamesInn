4285ac703284f795f24d78ce3528fff6ac3b4ae6
waliya15 <waliyahamid@gmail.com>
2022-02-26 18:32:50 +0500

    changes

--- frontend/src/modules/gamer/Requirements/Requirements.jsx
+++ frontend/src/modules/gamer/Requirements/Requirements.jsx
@@ -1,11 +1,15 @@
 import React,{useState} from 'react'
-import { Modal, Button, Form, Input} from 'antd';
+import { Modal, Button, Form, Input, Row, Col, Avatar, Card, Divider, Typography} from 'antd';
+import { Link } from 'react-router-dom'
+import { CheckCircleTwoTone,  } from '@ant-design/icons'
+import {data} from "./data";
 
 function Requirements() {
   const [visible, setVisible] = useState(false)
   const { Item }= Form;
   const { TextArea } = Input;
   const [form] = Form.useForm();
+  const {Text, Title}= Typography
 
   const showModal = () => {
     setVisible(true);
@@ -45,6 +49,45 @@ function Requirements() {
               </Item>
           </Form>
         </Modal>
+        <div className="posts">
+           <Row gutter={[10,10]} >
+           {data.map(post => (
+              <Col span={23} >
+                <Card
+                headStyle={{backgroundColor: '#213956', color:'white'}}
+                hoverable
+                title={post.title}
+                extra={post.isSold && <CheckCircleTwoTone style={{ fontSize: '25px'}} twoToneColor="#52c41a" /> }
+              >
+                <Row  >
+                  <Col style={{display:'flex', maxWidth:'70%'}} >
+                      <Col lg={2} md={4} sm={6} xs={8}>
+                        <Avatar src={post.image} size='large'  />
+                      </Col>
+                      <Col lg={22} md={20} sm={18} xs={16}>
+                       <Title level={5}>{post.gamerName}</Title>
+                       <Text strong>Gaming Account: {post.game}</Text>
+                       <br/>
+                      <Text>{post.description}</Text>
+                      </Col>
+                  </Col>
+                  <Col style={{maxHeight: '100%'}} >
+                    <Divider type="vertical" style={{height: '100%'}} />
+                  </Col>
+                  <div className="price" >
+                    <h2 style={{color:'inherit'}}>
+                      Price: Rs.{post.price}
+                    </h2>
+                  </div>
+                </Row>
+                <Link to={`/gamer/mypost/${post.postId}`}>More...</Link>
+              </Card>
+              </Col>
+            ))}
+           </Row>
+            
+            
+            </div>;
       </>
   )
 }
